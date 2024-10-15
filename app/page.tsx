4043b18eb8304528ae3bb316a678e064cae64d21
spkrish06 <sp.krish06@gmail.com>
2024-10-15 18:53:11 +0300

    Changed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <p className="text-6x ">Welcome to Our Platform</p>
+   <p className="text-6x ">Welcome to Mohar Chutiya</p>
   );
 }
