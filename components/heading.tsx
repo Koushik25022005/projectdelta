4e7ab035fb5a5684a4b3248b4f2d7c7c64c627c6
Koushik25022005 <sripathikoushik244@gmail.com>
2024-10-16 17:01:22 +0400

    formatting ui

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,16 +1,40 @@
+import { cn } from "@/lib/utils";
 import {LucideIcon} from "lucide-react";
 
 interface HeadingProps{
     title: string;
-    decription: string;
+    description: string;
     icon: LucideIcon;
     iconColor ?: string;
+    bgColor?: string;
 }
 
-export const Heading = () => {
+
+
+export const Heading = ({
+    title, 
+    description, 
+    icon: Icon, 
+    iconColor, 
+    bgColor
+}: HeadingProps) => {
     return (
-        <div>
-            Heading Component
+        <>
+        <div className="px-4 lg:px-8 items-center gap-x-3 md-8">
+            <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+            <Icon className={cn("w-10 h-10", iconColor)}/>
+            </div>
+
+            <div>
+            <h2 className="text-3xl font-bold ">
+            {title}
+            </h2>
+            <p>
+                {description}
+            </p>  
+            </div>
         </div>
+        
+        </>
     );
 };
