ea3e8dda815d0af9b6ceb0be555450d0eec73729
spkrish06 <sp.krish06@gmail.com>
2024-10-16 14:20:38 +0300

    Changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,10 @@
-import { UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dash Board page</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
