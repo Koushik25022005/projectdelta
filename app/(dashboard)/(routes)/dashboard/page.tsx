e733ac69b7fae3902fe844c74315e6903f91924e
maitymohar <maitymohar2005@gmail.com>
2024-10-16 17:14:37 +0530

    Changes done in the File

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,10 @@
 
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const DashboardPage = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <p>Dash Board page</p>
-=======
       <div className="mb-8 space-y-4">
       <h2 className="text-2xl md:text 4-xl font-bold">
         Explore the Power of AI
@@ -17,8 +15,7 @@ const DashboardPage = () => {
       </p>
       <div className="px-4 md:px-20 lg:px-32"></div>
       </div>
-      <UserButton afterSignOutUrl='/'/>
->>>>>>> a7b6fdfb4443ba27d86367edcbb0d97d4ffbf976
+     
     </div>
   )
 }
