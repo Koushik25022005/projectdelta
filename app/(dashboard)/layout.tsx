61c4116f57947ea77c47c25533ad3d11cb710d09
maitymohar <maitymohar2005@gmail.com>
2024-10-16 17:42:36 +0530

    Changes in the layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar"
+import Sidebar from "@/components/sidebar"
 
 
 const DashboardLayout = ({
@@ -9,7 +10,7 @@ const DashboardLayout = ({
     return(
         <div className="h-full relative ">
             <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] md:w-72 bg-gray-900">
-                <div>Hello Sidebar</div>
+                <div><Sidebar/></div>
             </div>
             <main className="md:pl-72">
             <Navbar/>
